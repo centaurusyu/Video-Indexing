10075594b2a5227047d2926dae6fb0d81f110c54
Yang <zhyants@gmail.com>
2023-04-29 13:09:55 -0700

    Update runPlayer.bat

--- runPlayer.bat
+++ runPlayer.bat
@@ -1,28 +1,15 @@
 @echo off
 
-REM  
-C:\Users\james\anaconda3\envs\576_project\python.exe SeceneShotDetectV2.py %1 %2
 
-<<<<<<< Updated upstream
-REM 
-=======
 REM call python program
 C:\ProgramData\anaconda3\python.exe SeceneShotDetectV2.py %1 %2
 
 REM wait for python finishing
->>>>>>> Stashed changes
 if %errorlevel% neq 0 (
   echo Python script execution failed.
   exit /b %errorlevel%
 )
 
-<<<<<<< Updated upstream
-REM 
-javac VideoPlayer.java
-
-REM 
-java VideoPlayer %1 %2
-=======
 REM compile java program
 javac VideoPlayer.java
 
@@ -34,4 +21,3 @@ del .\ShotList.txt
 del .\SceneList.txt
 del .\ShotSubshotList.txt
 rmdir .\tmpdata /s /q
->>>>>>> Stashed changes
